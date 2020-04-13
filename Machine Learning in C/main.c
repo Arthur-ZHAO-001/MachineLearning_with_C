c632192fd419c0ac5fd5376c1fa4a007066fb965
Chengliang-ZHAO <zcl271828@gmail.com>
2020-04-13 10:05:22 +0800

    Initial Commit

---
+++ Machine Learning in C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Machine Learning in C
+//
+//  Created by 赵呈亮 on 2020/4/13.
+//  Copyright © 2020 赵呈亮. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
